bfc5d71eac66484e6b11fbbb14e72cf68f2823ea
KzAlmaty <109860487+KzAlmaty@users.noreply.github.com>
2022-09-04 01:45:46 +0600

    First

---
+++ src/main/java/ru/orazbakov/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package ru.orazbakov.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.orazbakov.model.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
